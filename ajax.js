e11ed750cbca8fd938d82ec10894556c851c0e46
Rudy Pospisil <rudy@rudypospisil.com>
2018-04-29 23:58:28 -0400

    Additional updates.

--- ajax.js
+++ ajax.js
@@ -3,7 +3,7 @@ function loadEmployee() {
   // Find which employee the user selected.
   empId = document.getElementById("employeeDropdown").value;
   
-  // Create
+  // Execute query
   querySelect(empId);
   
   // Debugging...
@@ -17,9 +17,22 @@ function querySelect(empId) {
   
   reqObj.open("GET", reqUrl, false);
   
+  
   reqObj.send();
   
-  imgUrl = "img/" + reqObj.responseText;
+  jObj = JSON.parse(reqObj.response);
+  
+  imgUrl = "img/" + jObj[0]["profile_pic_filename"];
   
   document.getElementById("employeeProfilePic").src = imgUrl;
+  
+  document.getElementById("name").innerHTML = jObj[0]["name"];
+  document.getElementById("species").innerHTML = jObj[0]["species"];
+  document.getElementById("age").innerHTML = jObj[0]["age"];
+  document.getElementById("gender").innerHTML = jObj[0]["gender"];
+  document.getElementById("occupation").innerHTML = jObj[0]["occupation"];
+  document.getElementById("ethnicity").innerHTML = jObj[0]["ethnicity"];
+  document.getElementById("hair").innerHTML = jObj[0]["hair"];
+  document.getElementById("eyes").innerHTML = jObj[0]["eyes"];
+  
 }
