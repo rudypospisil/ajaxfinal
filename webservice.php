628f2f904ae2a84930eb0dc8a73fdf33a89345b3
Rudy Pospisil <rudy@rudypospisil.com>
2018-05-02 00:15:48 -0400

    Added UPDATE function.

--- webservice.php
+++ webservice.php
@@ -1,5 +1,5 @@
 <?php
-  
+  $queryType = $_GET['q'];
   $empId = $_GET['empId'];
   
   $dbUser = "rudyTesting";
@@ -11,13 +11,34 @@
       echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
   }
   
-  $sql = "SELECT name, species, age, gender, occupation, ethnicity, hair, eyes, profile_pic_filename FROM $dbTable WHERE ID = $empId";
-  $result = $mysqli->query($sql);
-  $row = $result->fetch_assoc();
-  $tempArray[] = $row;
-  
-  echo json_encode($tempArray);
-  
+  switch($queryType)
+  {
+    case "select" :
+      $sql = "SELECT fullName, species, age, gender, occupation, ethnicity, hair, eyes, profilePicFilename FROM $dbTable WHERE ID = $empId";
+      $result = $mysqli->query($sql);
+      $row = $result->fetch_assoc();
+      $tempArray[] = $row;
+      echo(json_encode($tempArray));
+      break;
+    case "update" :
+      $sql = 'UPDATE $dbTable SET fullName = "' . $_POST['fullName'] . '" WHERE ID = ' . $empId;
+      if($mysqli->query($sql))
+      {
+        echo $sql;
+        var_dump($_POST);
+        echo("Successful update");
+      } else 
+        {
+        echo $sql;
+        var_dump($_POST);
+        echo("Unsuccessful update.");
+      }
+      break;
+    default:
+      break;
+  }
+
+
   $result->close();
   $mysqli->close();
 ?>
